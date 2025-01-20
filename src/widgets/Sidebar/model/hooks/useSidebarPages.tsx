110069e27b44bd77b2f5299e2d894e2b78ffa0d0
Alexander Labchuk <lab4uk2014@yandex.ru>
2025-01-20 15:12:25 +0500

    Fix sidebar

--- src/widgets/Sidebar/model/hooks/useSidebarPages.tsx
+++ src/widgets/Sidebar/model/hooks/useSidebarPages.tsx
@@ -2,12 +2,7 @@ import { ReactNode } from "react"
 import { Tabs } from "../slices/tabsSlice"
 import { SendCameraConfig } from "features/SendCameraConfig"
 import { SendDbConfig } from "features/SendDbConfig"
-import {
-    DataAvgDelay,
-    DataAvgSpeed,
-    DataDensity,
-    DataTypes,
-} from "widgets/Data"
+import { DataAvgDelay, DataAvgSpeed, DataDensity, DataTypes } from "widgets/Data"
 import { DataComposition } from "widgets/Data/ui/DataComposition/DataComposition"
 import { DataIntensity } from "widgets/Data/ui/DataIntensity/DataIntensity"
 import { SendMarkupConfig } from "features/SendMarkupConfig"
@@ -15,68 +10,63 @@ import { EditLoginAndPassword } from "features/EditLoginAndPassword/ui/EditLogin
 import { useTypedSelector } from "shared/hooks/useTypedSelector"
 
 interface SidebarPages {
-    tabName: Tabs
-    title: string
-    blocks: {
-        header: string
-        element: ReactNode
-        hide?: boolean
-    }[]
+	tabName: Tabs
+	title: string
+	blocks: {
+		header: string
+		element: ReactNode
+		hide?: boolean
+	}[]
 }
 
 const DataList = () => (
-    <>
-        <DataTypes />
-        <DataComposition />
-        <DataIntensity />
-        <DataAvgSpeed />
-        <DataDensity />
-        <DataAvgDelay />
-    </>
+	<>
+		<DataTypes />
+		<DataComposition />
+		<DataIntensity />
+		<DataAvgSpeed />
+		<DataDensity />
+		<DataAvgDelay />
+	</>
 )
 
 const useSidebarPages = () => {
-    const { showChangeLoginAndPassword } = useTypedSelector(
-        (state) => state.appConfig
-    )
-    const { role } = useTypedSelector((state) => state.user)
+	const { showChangeLoginAndPassword } = useTypedSelector(state => state.appConfig)
+	const sidebarPages: SidebarPages[] = [
+		{
+			tabName: "data",
+			title: "Данные",
+			blocks: [{ header: "Данные", element: <DataList /> }],
+		},
+		{
+			tabName: "settings",
+			title: "Настройки",
+			blocks: [
+				{ header: "Настройки камеры", element: <SendCameraConfig /> },
+				{
+					header: "Параметры репликации на удаленную базу данных",
+					element: <SendDbConfig />,
+				},
+				{
+					header: "Настройки интерфейса управления",
+					element: <EditLoginAndPassword />,
+					hide: !showChangeLoginAndPassword,
+				},
+			],
+		},
+		{
+			tabName: "shot",
+			title: "Стоп-кадр",
+			blocks: [
+				{
+					header: "Настройки разметки",
+					element: <SendMarkupConfig />,
+				},
+			],
+		},
+	]
 
-    const sidebarPages: SidebarPages[] = [
-        {
-            tabName: "data",
-            title: "Данные",
-            blocks: [{ header: "Данные", element: <DataList /> }],
-        },
-        {
-            tabName: "settings",
-            title: "Настройки",
-            blocks: [
-                { header: "Настройки камеры", element: <SendCameraConfig /> },
-                {
-                    header: "Параметры репликации на удаленную базу данных",
-                    element: <SendDbConfig />,
-                    hide: role !== "admin",
-                },
-                {
-                    header: "Настройки интерфейса управления",
-                    element: <EditLoginAndPassword />,
-                    hide: !showChangeLoginAndPassword || role !== "admin",
-                },
-            ],
-        },
-        {
-            tabName: "shot",
-            title: "Стоп-кадр",
-            blocks: [
-                {
-                    header: "Настройки разметки",
-                    element: <SendMarkupConfig />,
-                },
-            ],
-        },
-    ]
-
-    return sidebarPages
+	return sidebarPages
 }
 
 export default useSidebarPages
